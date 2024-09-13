63b609ade2d563ec8223be4f4968b069976c3214
FatFrog <joaquin.sellanes@estudiantes.utec.edu.uy>
2024-09-13 17:08:32 -0300

    a

---
+++ practico1-master/DAL/DBContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DAL
+{
+    public class DBContext : DbContext
+    {
+        //string connectionString = "Server=SantinoLaptop\\SQLEXPRESS;Database=Practico1Seguir;Integrated Security=True;TrustServerCertificate=True";
+        string connectionString = "Server=sqlserver;Database=Practico1Seguir;User id=sa; Password=Abc*123!;TrustServerCertificate=True";
+
+        public DBContext() { }
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        { 
+            base.OnModelCreating(modelBuilder); 
+        }
+
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+
+        public static void UpdateDatabase()
+        {
+            using (var context = new DBContext())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
---
+++ practico1-master/Shared/Vehiculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Vehiculo
+    {
+        public long Id { get; set; }
+
+        public string Marca { get; set; } = "";
+
+        public string Modelo { get; set; } = "";
+
+        public string Matricula { get; set; } = "";
+
+        public long PersonaId { get; set; }
+
+        public virtual Persona Persona { get; set; } = new Persona();
+    }
+}
